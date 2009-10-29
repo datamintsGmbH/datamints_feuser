f1a0ab26e56fe664f42718627eae72de62774877
Bernhard Baumgartl <b.baumgartl@datamints.com>
2009-10-29 09:15:08 +0000

    First import.

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "datamints_feuser".
+#
+# Auto generated 29-10-2009 10:12
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'datamints feuser',
+	'description' => 'A new feuser register and edit extension.',
+	'category' => 'plugin',
+	'author' => 'Bernhard baumgartl',
+	'author_email' => 'b.baumgartl@datamints.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"264d";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"300e";s:14:"ext_tables.php";s:4:"8969";s:13:"locallang.xml";s:4:"90bb";s:16:"locallang_db.xml";s:4:"c58e";s:19:"doc/wizard_form.dat";s:4:"0eda";s:20:"doc/wizard_form.html";s:4:"9664";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:36:"pi1/class.tx_datamintsfeuser_pi1.php";s:4:"54d3";s:44:"pi1/class.tx_datamintsfeuser_pi1_wizicon.php";s:4:"7d71";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"1b9b";}',
+);
+
+?>
