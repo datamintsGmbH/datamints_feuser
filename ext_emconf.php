c162b3154f070dd0bd5f606d2a2abcb1db2ae237
Bernhard Baumgartl <b.baumgartl@datamints.com>
2009-11-17 10:22:30 +0000

    Version 1.0.0 Angleich der Dateihashes.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext "datamints_feuser".
+# Extension Manager/Repository config file for ext: "datamints_feuser"
 #
-# Auto generated 13-11-2009 14:02
+# Auto generated 17-11-2009 11:20
 #
 # Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:24:{s:9:"ChangeLog";s:4:"264d";s:10:"README.txt";s:4:"ee2d";s:35:"class.tx_flexform_getFieldNames.php";s:4:"e1e2";s:27:"datamints_feuser_README.txt";s:4:"251a";s:12:"ext_icon.gif";s:4:"21a8";s:17:"ext_localconf.php";s:4:"300e";s:15:"ext_php_api.dat";s:4:"e3d0";s:14:"ext_tables.php";s:4:"d932";s:14:"ext_tables.sql";s:4:"75c7";s:21:"flexform_data_pi1.xml";s:4:"3562";s:13:"locallang.xml";s:4:"8637";s:16:"locallang_db.xml";s:4:"c40a";s:19:"doc/wizard_form.dat";s:4:"a882";s:20:"doc/wizard_form.html";s:4:"3a3a";s:14:"pi1/ce_wiz.gif";s:4:"b093";s:36:"pi1/class.tx_datamintsfeuser_pi1.php";s:4:"8d9b";s:44:"pi1/class.tx_datamintsfeuser_pi1_wizicon.php";s:4:"7d71";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"acc7";s:24:"pi1/static/constants.txt";s:4:"d41d";s:20:"pi1/static/setup.txt";s:4:"b87a";s:20:"res/arrow_red_up.png";s:4:"4c36";s:24:"res/datamints_feuser.css";s:4:"1c6a";s:30:"res/datamints_feuser_mail.html";s:4:"9928";}',
+	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"264d";s:10:"README.txt";s:4:"ee2d";s:35:"class.tx_flexform_getFieldNames.php";s:4:"e1e2";s:27:"datamints_feuser_README.txt";s:4:"251a";s:12:"ext_icon.gif";s:4:"21a8";s:17:"ext_localconf.php";s:4:"300e";s:15:"ext_php_api.dat";s:4:"e3d0";s:14:"ext_tables.php";s:4:"d932";s:14:"ext_tables.sql";s:4:"75c7";s:21:"flexform_data_pi1.xml";s:4:"3562";s:13:"locallang.xml";s:4:"8637";s:16:"locallang_db.xml";s:4:"a260";s:14:"doc/manual.sxw";s:4:"d59c";s:19:"doc/wizard_form.dat";s:4:"1549";s:20:"doc/wizard_form.html";s:4:"814a";s:20:"res/arrow_red_up.png";s:4:"4c36";s:24:"res/datamints_feuser.css";s:4:"1c6a";s:30:"res/datamints_feuser_mail.html";s:4:"85bb";s:14:"pi1/ce_wiz.gif";s:4:"b093";s:36:"pi1/class.tx_datamintsfeuser_pi1.php";s:4:"4c5b";s:44:"pi1/class.tx_datamintsfeuser_pi1_wizicon.php";s:4:"7d71";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"2925";s:24:"pi1/static/constants.txt";s:4:"d41d";s:20:"pi1/static/setup.txt";s:4:"e521";}',
 );
 
 ?>
