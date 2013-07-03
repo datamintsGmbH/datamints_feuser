514f016c4ed71c75da39e0336336299ad03f0dff
Bernhard Baumgartl <b.baumgartl@datamints.com>
2013-07-03 13:48:59 +0000

    Updated the documentation. Updated the language files. Removed "cms" dependenciy from "ext_emconf.php".

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,6 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'cms' => '',
 			'php' => '5.3.0-0.0.0',
 			'typo3' => '4.5.0-6.1.99',
 		),
