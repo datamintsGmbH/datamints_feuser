ff8b1d753a3fb2010a8e1a32aac1488a4bedfa3c
Bernhard Baumgartl <b.baumgartl@datamints.com>
2014-02-18 15:35:23 +0100

    Added send mail approval to disapproval e-mail shipping.

--- pi1/class.tx_datamintsfeuser_pi1.php
+++ pi1/class.tx_datamintsfeuser_pi1.php
@@ -1489,7 +1489,13 @@ class tx_datamintsfeuser_pi1 extends tslib_pibase {
 
 			// Wenn der User deaktiviert wird, eine Account-Abgelehnt Mail senden (wenn User ablehnt an den Administrator, oder andersrum).
 			if (!$this->getConfigurationByShowtype(self::specialfieldKeyUserdelete)) {
-				$this->sendMail($this->userId, 'disapproval', !$this->isAdminMail($approvalType), $this->getConfigurationByShowtype());
+				if ($this->getConfigurationByShowtype('sendadminmail') && !$this->isAdminMail($approvalType)) {
+					$this->sendMail($this->userId, 'disapproval', TRUE, $this->getConfigurationByShowtype());
+				}
+
+				if ($this->getConfigurationByShowtype('sendusermail') && $this->isAdminMail($approvalType)) {
+					$this->sendMail($this->userId, 'disapproval', FALSE, $this->getConfigurationByShowtype());
+				}
 			}
 
 			// Ausgabe vorbereiten.
